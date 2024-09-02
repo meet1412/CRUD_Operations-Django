31cd0bed1be623215a2e9270e1a7e4506ba89270
Meet Patel <146736167+meet1412@users.noreply.github.com>
2024-09-02 19:06:52 -0400

    fucked up

--- crud/home/admin.py
+++ crud/home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from crud.home.models import Details
-
+from home.models import Details
 
 # Register your models here.
 admin.site.register(Details)
--- crud2/home/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from home.models import Details
-
-# Register your models here.
-admin.site.register(Details)
